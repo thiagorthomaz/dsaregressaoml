54cab5c37ae233dbcaeda00ab1bed96a75999078
Thiago Thomaz <thiagorthomaz@gmail.com>
2021-10-08 15:31:44 -0300

    Corrigido build

---
+++ Bicicleta_Demanda_Treino/Program.cs
@@ -0,0 +1,36 @@
+﻿using Bicicleta_Demanda_Treino.Classes;
+using Microsoft.ML;
+using System;
+using System.IO;
+
+namespace Bicicleta_Demanda_Treino
+{
+    class Program
+    {
+
+
+        static readonly string _dadosTreinoPath = Path.Combine(Environment.CurrentDirectory, "Dados/day.csv");
+
+        static readonly string _dadosTestePath = Path.Combine(Environment.CurrentDirectory, "Dados/Teste_day.csv");
+
+        static MLContext mLContext;
+
+        static void Main(string[] args)
+        {
+
+            if (!File.Exists(_dadosTreinoPath))
+            {
+                Console.WriteLine("Datasets não encontrados!");
+                return;
+            }
+
+            mLContext = new MLContext(seed: 34);
+
+            var treinoDataView = mLContext.Data.LoadFromTextFile<BikeHoraInstancia>(_dadosTreinoPath, separatorChar: ',', hasHeader: true);
+
+
+
+            Console.WriteLine("Programa finalizado!");
+        }
+    }
+}
